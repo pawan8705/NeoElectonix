650245ab8e52d5e9f7b6a9a2a743a3d3996120e9
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2025-06-21 10:16:11 +0530

    Update ResponsiveMenu.jsx

--- NeoElectronix/src/components/ResponsiveMenu.jsx
+++ NeoElectronix/src/components/ResponsiveMenu.jsx
@@ -1,6 +1,7 @@
-import { UserButton, useUser } from '@clerk/clerk-react'
+import { SignedIn, SignedOut, SignInButton, UserButton, useUser } from '@clerk/clerk-react'
 import { FaUserCircle } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+
 
 const ResponsiveMenu = ({ openNav, setOpenNav }) => {
     const { user } = useUser()
@@ -8,9 +9,16 @@ const ResponsiveMenu = ({ openNav, setOpenNav }) => {
         <div className={`${openNav ? "left-0" : "-left-[100%]"} fixed bottom-0 top-0 z-20 flex h-screen w-[75%] flex-col justify-between bg-[#efeeea] dark:bg-[#222222] px-8 pb-6 pt-16 text-black md:hidden rounded-r-xl shadow-md transition-all`}>
             <div>
                 <div className='flex items-center dark:text-white justify-start gap-3'>
-                    {
-                        user ? <UserButton size={50} className=' dark:text-white' /> : <FaUserCircle size={50} className=' dark:text-white' />
-                    }
+                    <div className='md:block'>
+                        <SignedOut>
+                            <SignInButton size={50} >
+                                <FaUserCircle size={50}></FaUserCircle>
+                            </SignInButton>
+                        </SignedOut>
+                        <SignedIn>
+                            <UserButton />
+                        </SignedIn>
+                    </div>
                     <div>
                         <h1>Hello, {user?.firstName}</h1>
                         <h1 className='text-sm text-slate-500 dark:text-[#c8c8c8]'>Premium User</h1>
