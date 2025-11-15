825def317cbdce7a0a4bfed9700d53a56ac154b7
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2025-11-15 18:55:53 +0530

    Update API endpoint for fetching products

--- NeoElectronix/src/context/DataContext.jsx
+++ NeoElectronix/src/context/DataContext.jsx
@@ -11,7 +11,7 @@ export const DataProvider = ({ children }) => {
     // fetching all products from api
     const fetchAllProducts = async () => {
         try {
-           const res = await axios.get('https://fakestoreapi.in/api/products?limit=150')
+           const res = await axios.get('https://fakestoreapi.com/products?limit=150')
            console.log(res);
            const productsData = res.data.products
            setData(productsData)
