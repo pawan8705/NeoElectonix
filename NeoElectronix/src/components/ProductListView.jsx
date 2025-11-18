77dbe6a22a51550ef6cc87cfd4fe6fd8d36e1314
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2025-11-18 22:37:22 +0530

    Update ProductListView.jsx

--- NeoElectronix/src/components/ProductListView.jsx
+++ NeoElectronix/src/components/ProductListView.jsx
@@ -9,7 +9,7 @@ const ProductListView = ({product}) => {
   return (
     <div className='space-y-4 mt-2 rounded-md'>
       <div className='bg-gray-100 flex gap-7 items-center p-2 rounded-md'>
-        <img src={product.image} alt={product.title} className='md:h-60 md:w-60 h-25 w-25 rounded-md cursor-pointer' onClick={()=>navigate(`/products/${product.id}`)}/>
+        <img src={product.images} alt={product.title} className='md:h-60 md:w-60 h-25 w-25 rounded-md cursor-pointer' onClick={()=>navigate(`/products/${product.id}`)}/>
         <div className='space-y-2'>
           <h1 className='font-bold md:text-xl text-lg line-clamp-3 hover:text-red-400 md:w-full w-[220px]'>{product.title}</h1>
           <p className='font-semibold flex items-center md:text-lg text-sm'>$<span className='md:text-4xl text-3xl'>{product.price}</span> ({product.discount}% off)</p>
