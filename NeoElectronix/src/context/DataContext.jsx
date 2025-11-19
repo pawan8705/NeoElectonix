70d2733c87680ea73fca1acd704897a5fc6fe50e
Pawan Tripathi <165460080+pawan8705@users.noreply.github.com>
2025-11-19 11:21:58 +0530

    Enhance DataProvider with image validation
    Updated DataContext to validate images before setting data and increased product limit.

--- NeoElectronix/src/context/DataContext.jsx
+++ NeoElectronix/src/context/DataContext.jsx
@@ -1,41 +1,74 @@
+/* eslint-disable react-refresh/only-export-components */
 /* eslint-disable react-hooks/rules-of-hooks */
 import axios from "axios";
 import { createContext, useContext, useState } from "react";
 
-// eslint-disable-next-line react-refresh/only-export-components
 export const DataContext = createContext(null);
 
 export const DataProvider = ({ children }) => {
-    const [data, setData] = useState()
+    const [data, setData] = useState([]);
 
-    // fetching all products from api
+    // IMAGE VALIDATION FUNCTION
+    const validateImage = (url) => {
+        return new Promise((resolve) => {
+            const img = new Image();
+            img.src = url;
+
+            img.onload = () => resolve(true);
+            img.onerror = () => resolve(false);
+        });
+    };
+
+    // FETCHING ALL PRODUCTS
     const fetchAllProducts = async () => {
         try {
-           const res = await axios.get('https://grsdfdqqyaqayzjlqxcx.supabase.co/functions/v1/products?limit=600')
-           console.log(res);
-           const productsData = res.data
-           setData(productsData)
-           
+            const res = await axios.get(
+                "https://grsdfdqqyaqayzjlqxcx.supabase.co/functions/v1/products?limit=800"
+            );
+
+            let productsData = res.data;
+
+            // FILTER VALID IMAGE PRODUCTS
+            const validatedProducts = [];
+
+            for (let product of productsData) {
+                const isValid = await validateImage(product.thumbnail);
+                if (isValid) {
+                    validatedProducts.push(product);
+                }
+            }
+
+            setData(validatedProducts);
+
         } catch (error) {
             console.log(error);
-
         }
-    }
+    };
 
-    const getUniqueCategory = (data, property) =>{
-        let newVal = data?.map((curElem) =>{
-            return curElem[property]
-        })
-        newVal = ["All",...new Set(newVal)]
-        return newVal
-      }
-    
-      const categoryOnlyData = getUniqueCategory(data, "category")
-      const brandOnlyData = getUniqueCategory(data, "brand")
-    return <DataContext.Provider value={{ data, setData,fetchAllProducts, categoryOnlyData, brandOnlyData }}>
-        {children}
-    </DataContext.Provider>
-}
+    const getUniqueCategory = (data, property) => {
+        let newVal = data?.map((curElem) => {
+            return curElem[property];
+        });
+        newVal = ["All", ...new Set(newVal)];
+        return newVal;
+    };
 
-// eslint-disable-next-line react-refresh/only-export-components
-export const getData = ()=> useContext(DataContext)
+    const categoryOnlyData = getUniqueCategory(data, "category");
+    const brandOnlyData = getUniqueCategory(data, "brand");
+
+    return (
+        <DataContext.Provider
+            value={{
+                data,
+                setData,
+                fetchAllProducts,
+                categoryOnlyData,
+                brandOnlyData,
+            }}
+        >
+            {children}
+        </DataContext.Provider>
+    );
+};
+
+export const getData = () => useContext(DataContext);
